865ea6fb9f90452b121d11caa1e3e06d52e6c514
takudzwa1998 <shumbamhinit@gmail.com>
2020-09-11 05:52:49 +0200

    9/11 Edits

--- BE_routes/api.js
+++ BE_routes/api.js
@@ -1,22 +1,89 @@
 const express = require('express');
-
 const router = new express.Router();
 const Buoy_One = require('../BE_buoy_models/Buoy_One');
+var prependFile = require('prepend-file');
 var fs = require('fs');
 var http = require('http');
 
 //routes
-router.post('/save', (req, res)=>{
+router.post('/save_buoy', (req, res)=>{
+  console.log(req.body.buoy_tag);
+/*  Buoy_One.find({ })
+  .then((data)=>{
+    console.log('Data ', data);
+    res.json(data);
+  })
+  .catch((error)=>{
+    console.log('error', error)
 
-  fs.appendFile('BE_routes/api.js', 'Beast Coast= '+req.body.buoy_tag+' JOEY BADA$$', function (err,data) {
+  */
+/*  const data={
+    GPS_Location: 'This is the GPS Location',
+    temperature: 65
+  };*/
+
+  /*const new_Buoy_One = new Buoy_One(data);//instance of model */
+
+/*  new_Buoy_One.save((error)=>{
+    if (error){
+      console.log("Error: Something happened, data not saved");
+      return;
+    }
+    return res.json({msg:"Data is saved to Mongo"});
+  }); */
+
+  //create new file for new buoy
+  fs.appendFile('BE_buoy_models/'+req.body.buoy_tag+'.js',
+    "const mongoose = require('mongoose');"+"\n"+
+    "const Schema = mongoose.Schema;"+"\n"+"\n"+
+    "const BuoySchema = new Schema({"+"\n"+
+    req.body.param1+":"+"String,"+"\n"+
+    req.body.param2+":"+"String,"+"\n"+
+    req.body.param3+":"+"String,"+"\n"+
+    "gps_location"+":"+"String"+"});\n"+
+    "const "+req.body.buoy_tag+" = mongoose.model("+"\'"+req.body.buoy_tag+"\'"+", BuoySchema);"+"\n"+
+    "module.exports = "+req.body.buoy_tag+";"
+   , function (err,data) {
     if (err) {
       return console.log(err);
     }
-    console.log(data);
+    console.log("Append File Data ", data);
   });
+  //add declaration line to api.js
+  prependFile('BE_routes/api.js',
+  'const '+req.body.buoy_tag+' = '+
+  'require(\'../BE_buoy_models/'+req.body.buoy_tag+'\');\n',
+  function(error){
+    if (error){
+      console.log(error);
+    }
+  }
+  );
 
-  console.log("Body: ", req.body.buoy_tag);
-  //console.log("Data received: ", res.data);
+  //var new_collection_name ="new_"+String(req.body.buoy_tag);
+  const Latest_buoy = require('../BE_buoy_models/'+req.body.buoy_tag);
+  const new_collection_name =new Latest_buoy(req.body);
+
+/*  new_collection_name.save((error)=>{
+      if (error){
+        console.log("Error: Something happened, data not saved");
+        return;
+      }
+      return res.json({msg:"Data is saved to Mongo"});
+    });*/
+
+
+/*  fs.appendFile('./BE_routes/api.js',
+  'const '+req.body.buoy_tag+' = '+
+  'require(\'../BE_buoy_models/'+req.body.buoy_tag+'\');\n'
+  , function (err,data) {
+   if (err) {
+     return console.log(err);
+   }
+   console.log("Append File Data ", data);
+ });*/
+
+  console.log("Received Body: ", req.body);
   res.json({
     msg:"data received"
   })
--- src/buoy_form.js
+++ src/buoy_form.js
@@ -3,38 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
 
-
 class Buoy_Form extends React.Component{
 
   state={
-    buoy_tag:''
+    buoy_tag:'',
+    rockblock_link:'',
+    param1:'',
+    param2:'',
+    param3:'',
+    gps_location:''
   };
 
-  stateChange=(event)=>{
-    const new_buoy = event.target;
-    const name = new_buoy.name;
-    const value = new_buoy.value;
-
-  this.setState({
-    [name]: value
-  });
-
+  stateChange=({target})=>{
+    const {name, value}=target;
+    this.setState({[name]: value});
   };
 
   submit=(event)=>{
     event.preventDefault();
 
     const load ={
-      buoy_tag:this.state.buoy_tag
+      buoy_tag:this.state.buoy_tag,
+      rockblock_link:this.state.rockblock_link,
+      param1:this.state.param1,
+      param2:this.state.param2,
+      param3:this.state.param3,
+      gps_location:this.state.gps_location
     };
 
     axios({
-      url:'/api/save',
+      url:'/api/save_buoy',
       method:'POST',
       data: load
     })
      .then(()=>{
        console.log("data sent");
+       this.resetInputFields();
      })
      .catch(()=>{
        console.log("data not sent");
@@ -42,11 +46,22 @@ class Buoy_Form extends React.Component{
 
   };
 
+  resetInputFields=()=>{
+    this.setState({
+      buoy_tag:'',
+      rockblock_link:'',
+      param1:'',
+      param2:'',
+      param3:'',
+      gps_location:''
+    });
+  };
+
   render(){
-    console.log("State", this.state.buoy_tag);
+    console.log("State", this.state);
     return(
       <div className="form-input">
-      <h2>Qwerty Keyboard is the best</h2>
+      <h2>Sample Buoy Form</h2>
           <form onSubmit={this.submit}>
               <div>
               <input
@@ -56,6 +71,41 @@ class Buoy_Form extends React.Component{
                 value={this.state.buoy_tag}
                 onChange={this.stateChange}
               />
+              <input
+                type="text"
+                name="rockblock_link"
+                placeholder= "RockBlock Link"
+                value={this.state.rockblock_link}
+                onChange={this.stateChange}
+              />
+              <input
+                type="text"
+                name="param1"
+                placeholder= "Parameter One"
+                value={this.state.param1}
+                onChange={this.stateChange}
+              />
+              <input
+                type="text"
+                name="param2"
+                placeholder= "Parameter Two"
+                value={this.state.param2}
+                onChange={this.stateChange}
+              />
+              <input
+                type="text"
+                name="param3"
+                placeholder= "Parameter Three"
+                value={this.state.param3}
+                onChange={this.stateChange}
+              />
+              <input
+                type="text"
+                name="gps_location"
+                placeholder= "GPS Location"
+                value={this.state.gps_location}
+                onChange={this.stateChange}
+              />
               </div>
             <button>Submit</button>
           </form>
@@ -63,7 +113,6 @@ class Buoy_Form extends React.Component{
       </div>
     );
   }
-
 
 }
 
